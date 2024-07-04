55ab1f30f83213595df41a3904a5dcd9ddf91795
beopiyo <benardopiyo13@gmail.com>
2024-07-04 19:30:48 +0300

    Refactor: adding comments to the file

--- tetrominosolver/input_reader.go
+++ tetrominosolver/input_reader.go
@@ -6,36 +6,40 @@ import (
 	"os"
 )
 
-// ReadInputFile reads the Tetromino shapes from a file
-func ReadInputFile(path string) ([][]string, error) {
-	file, err := os.Open(path)
+// ReadInputFile reads a file containing Tetromino shapes and returns them as a slice of 2D string slices.
+// The function will read the file, validate the format, and parse the Tetromino shapes into a slice of 2D string slices.
+
+func ReadInputFile(filePath string) ([][]string, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, fmt.Errorf("ERROR")
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var tetrominos [][]string
-	var currentTetromino []string
 
+	// Initialize a slice to hold the Tetromino grids.
+	Tetromino := [][]string{}
+	tetrominoLines := []string{}
 	for scanner.Scan() {
-		line := scanner.Text()
-		if len(line) == 4 {
-			currentTetromino = append(currentTetromino, line)
-		} else if line != "" {
+		currentLine := scanner.Text()
+		if len(currentLine) == 4 {
+			tetrominoLines = append(tetrominoLines, currentLine)
+		} else if currentLine != "" && len(currentLine) != 4 {
 			return nil, fmt.Errorf("ERROR")
 		}
 
-		if line == "" {
-			if len(currentTetromino) > 0 {
-				tetrominos = append(tetrominos, currentTetromino)
-				currentTetromino = []string{}
-			}
+		if currentLine == "" { // Indicate the end of a Tetromino grid
+			Tetromino = append(Tetromino, tetrominoLines)
+			tetrominoLines = []string{} // Reset the temporary slice for the next Tetromino grid.
+		} else if currentLine != "" && len(tetrominoLines) > 4 {
+			return nil, fmt.Errorf("ERROR")
 		}
 	}
-	if len(currentTetromino) > 0 {
-		tetrominos = append(tetrominos, currentTetromino)
+
+	if len(tetrominoLines) > 0 {
+		Tetromino = append(Tetromino, tetrominoLines)
 	}
 
-	return tetrominos, nil
+	return Tetromino, nil
 }
